0eda33083b9fddc6d80cd783bc26106bc4dd6709
Ian <ian.jolo08@gmail.com>
2025-06-18 23:54:36 -0300

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import { useState, useEffect } from "react";
 function App() {
   const [listaPokemon, setListaPokemon] = useState([]);
   const [pokemon, setPokemon] = useState(null);
-  const baseURL="https://pokeapi.co/api/v2/pokemon?limit=20"
+  const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=20";
+
   useEffect(() => {
     axios.get(baseURL).then((response) => {
-        setListaPokemon(response.data.results); 
-      });
+      setListaPokemon(response.data.results); 
+    });
   }, []);
 
   const mostrarDetalles = (urlPokemon) => {
@@ -21,19 +22,20 @@ function App() {
   return (
     <div>
       <h1>Lista de Pokemones</h1>
-        {listaPokemon.map((pokemon) => (
-<button onClick={() => mostrarDetalles(pokemon.url)}>
-{pokemon.name}
-</button>
-))}
+      {listaPokemon.map((pokemon) => (
+        <button key={pokemon.name} onClick={() => mostrarDetalles(pokemon.url)}>
+          {pokemon.name}
+        </button>
+      ))}
 
       {pokemon && (
-        <div>
-        <h1>{pokemon.name}</h1>
-        <p>Habilidades:</p>
-            {pokemon.abilities.map((obj) => (
-        <li>{obj.ability.name}</li>
-            ))}
+        <div className="pokemon-detalles">
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.sprites.front_default}/>
+          <h3>Habilidades:</h3>
+          {pokemon.abilities.map((obj, index) => (
+            <p key={index}>{obj.ability.name}</p>
+          ))}
         </div>
       )}
     </div>
